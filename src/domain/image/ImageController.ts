82df956ea9e44fad1b6ec85d236acb1f149891a0
qlido <67864410+qlido@users.noreply.github.com>
2023-02-15 23:09:49 +0900

    완성?

--- src/domain/image/ImageController.ts
+++ src/domain/image/ImageController.ts
@@ -3,15 +3,15 @@ import fs from "fs";
 import path from "path";
 import {NotFoundException} from "@global/exception/exceptions";
 import * as console from "console";
-
+require('dotenv').config()
 const express = require('express');
 const router = express.Router();
-
+const Folder = process.env.IMAGE_PATH!;
 router.get('/:postCode',(req:Request,res:Response,next:NextFunction) => {
     const postCode:string = req.params.postCode;
-    const File = fs.readdirSync(path.join("C:\\Users\\jason\\Image"))
+    const File = fs.readdirSync(path.join(Folder))
         .filter((file:string) => file.includes(postCode))[0]
-    if(File) return res.status(200).sendFile(path.join("C:\\Users\\jason\\Image",File))
+    if(File) return res.status(200).sendFile(path.join(Folder,File))
 
     else next(new NotFoundException())
 })
